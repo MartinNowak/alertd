641a8cd5af746b99403119da06d1740dbbf83b15
Martin Nowak <code@dawg.eu>
2017-12-29 16:24:37 +0100

    update deps

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -124,7 +124,6 @@ export class CheckDetails {
         yAxis: {
             title: {
                 text: '',
-                style: '{display: none;}'
             }
         },
         plotOptions: {
