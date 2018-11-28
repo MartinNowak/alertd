a55c4ef10fe2cc3dabdcdf6133194ccd444a4f81
Martin Nowak <code@dawg.eu>
2018-11-28 17:53:02 +0100

    fix pie by default

--- sqlite3.sh
+++ sqlite3.sh
@@ -3,6 +3,6 @@
 if [ ! -f sqlite3.o ]; then
     wget -c https://www.sqlite.org/2016/sqlite-amalgamation-3110000.zip
     unzip -o sqlite-amalgamation-3110000.zip sqlite-amalgamation-3110000/sqlite3.c
-    cc -O2 -c sqlite-amalgamation-3110000/sqlite3.c
+    cc -O2 -c -fPIC sqlite-amalgamation-3110000/sqlite3.c
     rm -rf sqlite-amalgamation-3110000
 fi
